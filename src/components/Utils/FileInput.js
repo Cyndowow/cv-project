5b7c62a33537c616c7439d621789aff30f5dafea
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2023-06-16 17:36:40 +0200

    Add File Input

--- src/components/Utils/FileInput.js
+++ src/components/Utils/FileInput.js
@@ -3,9 +3,8 @@ import React from "react";
 const FileInput = ({ label, name, onChange}) => {
     return(
         <label className="label">
-            <input className="file-input" type="file" onChange={onChange} name={name}>
-                {label}
-            </input>
+            <input className="file-input" type="file" onChange={onChange} name={name} />
+            {label}
         </label>
     )
 }
